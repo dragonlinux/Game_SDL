07dd236549a9af40f0d1133c552c8d95160e61d0
root <root@dragon_Intel_Q8200.(none)>
2013-08-24 21:08:00 +0800

    事件驱动编程

--- src/Game_my_first.cpp
+++ src/Game_my_first.cpp
@@ -6,133 +6,130 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-/*This source code copyrighted by Lazy Foo' Productions (2004-2013)
- and may not be redistributed without written permission.*/
-
 //The headers
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
 #include <string>
 
-//Screen attributes
+//窗口属性
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32;
 
-//The surfaces
+//表面
 SDL_Surface *image = NULL;
 SDL_Surface *screen = NULL;
 
-//The event structure that will be used
+//将要用到的事件结构体
 SDL_Event event;
 
 SDL_Surface *load_image(std::string filename)
 {
-	//The image that's loaded
+	//存储加载好的图像
 	SDL_Surface* loadedImage = NULL;
 
-	//The optimized image that will be used
+	//存储优化后的图像
 	SDL_Surface* optimizedImage = NULL;
 
-	//Load the image
+	//加载图像
 	loadedImage = IMG_Load(filename.c_str());
 
-	//If the image loaded
+	//如果加载成功
 	if (loadedImage != NULL)
 	{
-		//Create an optimized image
+		//创建优化的图像
 		optimizedImage = SDL_DisplayFormat(loadedImage);
 
-		//Free the old image
+		//释放原先加载的图像
 		SDL_FreeSurface(loadedImage);
 	}
 
-	//Return the optimized image
+	//返回优化的图像
 	return optimizedImage;
 }
 
 void apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* destination)
 {
-	//Temporary rectangle to hold the offsets
+	//存储偏移量的临时矩形
 	SDL_Rect offset;
 
-	//Get the offsets
+	//存入偏移量
 	offset.x = x;
 	offset.y = y;
 
-	//Blit the surface
+	//执行blit操作
 	SDL_BlitSurface(source, NULL, destination, &offset);
 }
 
 bool init()
 {
-	//Initialize all SDL subsystems
+	//初始化SDL的所有子系统
 	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
 	{
 		return false;
 	}
 
-	//Set up the screen
+	//设置窗口
 	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
 
-	//If there was an error in setting up the screen
+	//如果设置出错
 	if (screen == NULL)
 	{
 		return false;
 	}
 
-	//Set the window caption
-	SDL_WM_SetCaption("Event test", NULL);
+	//设置窗口标题
+	SDL_WM_SetCaption("Event test叶玉龙", NULL);
 
-	//If everything initialized fine
+	//如果所有初始化操作都成功
 	return true;
 }
 
 bool load_files()
 {
-	//Load the image
+	//加载图像
 	image = load_image("x.png");
 
-	//If there was an error in loading the image
+	//如果加载出错
 	if (image == NULL)
 	{
 		return false;
 	}
 
-	//If everything loaded fine
+	//如果所有图片加载正常
 	return true;
 }
 
 void clean_up()
 {
-	//Free the surface
+	//释放图像
 	SDL_FreeSurface(image);
 
-	//Quit SDL
+	//退出SDL
 	SDL_Quit();
 }
 
 int main(int argc, char* args[])
 {
-	//Make sure the program waits for a quit
+	//确保程序一直等待quit
 	bool quit = false;
 
-	//Initialize
+	//初始化
 	if (init() == false)
 	{
 		return 1;
 	}
 
-	//Load the files
+	//加载文件
 	if (load_files() == false)
 	{
 		return 1;
 	}
 
-	//Apply the surface to the screen
+	//将image表面应用到窗口上
 	apply_surface(0, 0, image, screen);
 
-	//Update the screen
+	//更新窗口
 	if (SDL_Flip(screen) == -1)
 	{
 		return 1;
@@ -144,16 +141,16 @@ int main(int argc, char* args[])
 		//当有事件发生时，我们需要处理它们
 		while (SDL_PollEvent(&event))
 		{
-			//If the user has Xed out the window
+			//如果用户点击了窗口右上角的关闭按钮
 			if (event.type == SDL_QUIT)
 			{
-				//Quit the program
+				//退出程序
 				quit = true;
 			}
 		}
 	}
 
-	//Free the surface and quit SDL
+	//清理表面并退出SDL
 	clean_up();
 
 	return 0;
